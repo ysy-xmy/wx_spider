e23246c855926566f1096e95803a5aa63426e4f3
ysy-xmy <52122343+ysy-xmy@users.noreply.github.com>
2024-04-11 17:22:02 +0800

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,18 @@
+# 定义全局变量
+
+wxgeturl_cookies = {
+#填写cookies
+}
+
+
+#要爬取的微信公众号
+wechat_accounts_fakeid={
+
+   # 如：'莞博社区':'Mzk0NTQ3Mzk5Nw==',
+
+}
+
+wx_token='1406298924'
+
+# proxies = {'http': '112.80.248.73'}
+
